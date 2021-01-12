137fd03b6af9c9f7fe1e6a646a436a636bd0adc9
Giorgos Logiotatidis <seadog@sealabs.net>
2021-01-12 16:10:30 +0200

    Copy assets.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
 
+  // Don't process folders with static assets e.g. images
+  eleventyConfig.addPassthroughCopy("content/assets");
+
   return {
     templateFormats: ["html"],
     pathPrefix: "/",
