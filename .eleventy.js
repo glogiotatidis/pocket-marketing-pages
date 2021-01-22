6325f3ed8a16411fc46433e2a1305fc997a08843
Giorgos Logiotatidis <seadog@sealabs.net>
2021-01-22 09:36:07 +0200

    Add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("content/assets");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
   return {
     templateFormats: ["html"],
